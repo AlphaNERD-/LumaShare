7a45414003ea16cea5f0426110af58e54f6a7bd6
Julia Wolf <lovelydeath94@googlemail.com>
2021-08-15 01:10:48 +0200

    Added source files, project files and stuff

---
+++ LumaShare/LumaShareRenderer.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Xml;
+
+namespace LumaShare
+{
+    public static class LumaSharepicRenderer
+    {
+        public static RenderTargetBitmap RenderSharepic(string pathToTopImage, string pathToBottomImage, Profile prof, BackgroundSettingEnum backgroundSettings, int border)
+        {
+            int width;
+            int height;
+            int leftOffset;
+            int topOffset;
+
+            BitmapImage imgProfile = GetBitmapImage(prof.ImageFileName);
+
+            width = imgProfile.PixelWidth + (border * 2);
+            height = imgProfile.PixelHeight + (border * 2);
+            leftOffset = border;
+            topOffset = border;
+
+            DrawingVisual dv = new DrawingVisual();
+            using (DrawingContext dc = dv.RenderOpen())
+            {
+                if (backgroundSettings != BackgroundSettingEnum.Transparent)
+                {
+                    string blurImagePath;
+
+                    if (backgroundSettings == BackgroundSettingEnum.TopScreenBlurred)
+                    {
+                        blurImagePath = pathToTopImage;
+                    }
+                    else
+                    {
+                        blurImagePath = pathToBottomImage;
+                        
+                    }
+
+                    if (File.Exists(blurImagePath))
+                    {
+                        BitmapImage imgBlur = GetBitmapImage(blurImagePath);
+
+                        float scalingFactor = 0;
+
+                        scalingFactor = (float)height / (float)imgBlur.PixelHeight;
+
+                        float targetWidth = imgBlur.PixelWidth * scalingFactor;
+                        float targetHeight = imgBlur.PixelHeight * scalingFactor;
+
+                        Bitmap tempBitmap = ConvertBitmapImageToBitmap(imgBlur);
+
+                        Bitmap blurBitmap = Blur(tempBitmap, new Rectangle(0, 0, tempBitmap.Width, tempBitmap.Height), 3);
+
+                        BitmapImage blurImage = ConvertBitmapToBitmapImage(blurBitmap);
+
+                        dc.DrawImage(blurImage, new Rect(((targetWidth - width) / 2) * -1, 0, targetWidth, targetHeight));
+                    }
+                }
+
+                dc.DrawImage(imgProfile, new Rect(leftOffset, topOffset, imgProfile.PixelWidth, imgProfile.PixelHeight));
+
+                if (File.Exists(pathToTopImage))
+                {
+                    BitmapImage imgTop = GetBitmapImage(pathToTopImage);
+                    dc.DrawImage(imgTop, new Rect(prof.TopScreenStart.X + leftOffset, prof.TopScreenStart.Y + topOffset, prof.TopScreenSize.Width, prof.TopScreenSize.Height));
+                }
+                else
+                {
+                    dc.DrawRectangle(System.Windows.Media.Brushes.Black, null, new Rect(prof.TopScreenStart.X + leftOffset, prof.TopScreenStart.Y + topOffset, prof.TopScreenSize.Width, prof.TopScreenSize.Height));
+                }
+
+                if (File.Exists(pathToBottomImage))
+                {
+                    BitmapImage imgTop = GetBitmapImage(pathToBottomImage);
+                    dc.DrawImage(imgTop, new Rect(prof.BottomScreenStart.X + leftOffset, prof.BottomScreenStart.Y + topOffset, prof.BottomScreenSize.Width, prof.BottomScreenSize.Height));
+                }
+                else
+                {
+                    dc.DrawRectangle(System.Windows.Media.Brushes.Black, null, new Rect(prof.BottomScreenStart.X + leftOffset, prof.BottomScreenStart.Y + topOffset, prof.BottomScreenSize.Width, prof.BottomScreenSize.Height));
+                }
+            }
+
+            RenderTargetBitmap rtb = new RenderTargetBitmap(width, height, 96, 96, PixelFormats.Pbgra32);
+            rtb.Render(dv);
+
+            return rtb;
+        }
+
+        private static BitmapImage GetBitmapImage(string imageFile)
+        {
+            BitmapImage src = new BitmapImage();
+            src.BeginInit();
+            src.UriSource = new Uri(imageFile, UriKind.Absolute);
+            src.CacheOption = BitmapCacheOption.OnLoad;
+            src.EndInit();
+
+            return src;
+        }
+
+        private static BitmapImage ConvertBitmapToBitmapImage(Bitmap bitmap)
+        {
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+
+                return bitmapImage;
+            }
+        }
+
+        public static System.Drawing.Bitmap ConvertBitmapImageToBitmap(BitmapImage bmpImg)
+        {
+            if (bmpImg == null)
+                return null;
+
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bmpImg));
+                enc.Save(outStream);
+                return new System.Drawing.Bitmap(outStream);
+            }
+        }
+
+        private static Bitmap Blur(Bitmap image, Rectangle rectangle, Int32 blurSize)
+        {
+            Bitmap blurred = new Bitmap(image.Width, image.Height);
+
+            // make an exact copy of the bitmap provided
+            using (Graphics graphics = Graphics.FromImage(blurred))
+                graphics.DrawImage(image, new Rectangle(0, 0, image.Width, image.Height),
+                    new Rectangle(0, 0, image.Width, image.Height), GraphicsUnit.Pixel);
+
+            // look at every pixel in the blur rectangle
+            for (Int32 xx = rectangle.X; xx < rectangle.X + rectangle.Width; xx++)
+            {
+                for (Int32 yy = rectangle.Y; yy < rectangle.Y + rectangle.Height; yy++)
+                {
+                    Int32 avgR = 0, avgG = 0, avgB = 0;
+                    Int32 blurPixelCount = 0;
+
+                    // average the color of the red, green and blue for each pixel in the
+                    // blur size while making sure you don't go outside the image bounds
+                    for (Int32 x = xx; (x < xx + blurSize && x < image.Width); x++)
+                    {
+                        for (Int32 y = yy; (y < yy + blurSize && y < image.Height); y++)
+                        {
+                            System.Drawing.Color pixel = blurred.GetPixel(x, y);
+
+                            avgR += pixel.R;
+                            avgG += pixel.G;
+                            avgB += pixel.B;
+
+                            blurPixelCount++;
+                        }
+                    }
+
+                    avgR = avgR / blurPixelCount;
+                    avgG = avgG / blurPixelCount;
+                    avgB = avgB / blurPixelCount;
+
+                    // now that we know the average for the blur size, set each pixel to that color
+                    for (Int32 x = xx; x < xx + blurSize && x < image.Width && x < rectangle.Width; x++)
+                        for (Int32 y = yy; y < yy + blurSize && y < image.Height && y < rectangle.Height; y++)
+                            blurred.SetPixel(x, y, System.Drawing.Color.FromArgb(avgR, avgG, avgB));
+                }
+            }
+
+            return blurred;
+        }
+    }
+
+    public class Profile
+    {
+        public string ProfileName { get; set; }
+        public string ImageFileName;
+        public System.Windows.Point TopScreenStart;
+        public System.Windows.Size TopScreenSize;
+        public System.Windows.Point BottomScreenStart;
+        public System.Windows.Size BottomScreenSize;
+
+        public Profile(string profileXmlFile)
+        {
+            XmlDocument docProfile = new XmlDocument();
+            docProfile.Load(profileXmlFile);
+
+            XmlNode rootNode = docProfile.ChildNodes.Item(0);
+            ProfileName = rootNode.Attributes.GetNamedItem("Name").InnerText;
+
+            string directory = Path.GetDirectoryName(profileXmlFile);
+            ImageFileName = Path.Combine(directory, rootNode.Attributes.GetNamedItem("ImageFileName").InnerText);
+
+            XmlNode nodeTopScreen = rootNode.ChildNodes.Item(0);
+            string[] topScreenStart = nodeTopScreen.Attributes.GetNamedItem("Start").InnerText.Split(',');
+            TopScreenStart = new System.Windows.Point(Convert.ToInt32(topScreenStart[0]), Convert.ToInt32(topScreenStart[1]));
+
+            string[] topScreenSize = nodeTopScreen.Attributes.GetNamedItem("Size").InnerText.Split(',');
+            TopScreenSize = new System.Windows.Size(Convert.ToInt32(topScreenSize[0]), Convert.ToInt32(topScreenSize[1]));
+
+            XmlNode nodeBottomScreen = rootNode.ChildNodes.Item(1);
+            string[] bottomScreenStart = nodeBottomScreen.Attributes.GetNamedItem("Start").InnerText.Split(',');
+            BottomScreenStart = new System.Windows.Point(Convert.ToInt32(bottomScreenStart[0]), Convert.ToInt32(bottomScreenStart[1]));
+
+            string[] bottomScreenSize = nodeBottomScreen.Attributes.GetNamedItem("Size").InnerText.Split(',');
+            BottomScreenSize = new System.Windows.Size(Convert.ToInt32(bottomScreenSize[0]), Convert.ToInt32(bottomScreenSize[1]));
+        }
+    }
+
+    public enum BackgroundSettingEnum
+    {
+        Transparent,
+        TopScreenBlurred,
+        BottomScreenBlurred
+    }
+}
---
+++ LumaShare/MainWindow.xaml.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LumaShare
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private bool allowConfigUpdate = true;
+        private string configFileName = System.IO.Path.Combine(Environment.CurrentDirectory, "config.txt");
+
+        private List<Profile> profiles = new List<Profile>();
+        public List<Profile> Profiles
+        {
+            get
+            {
+                return profiles;
+            }
+            set
+            {
+                profiles = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Profiles)));
+            }
+        }
+
+        private Profile selectedProfile;
+        public Profile SelectedProfile
+        {
+            get
+            {
+                return selectedProfile;
+            }
+            set
+            {
+                selectedProfile = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedProfile)));
+                UpdateConfig();
+                UpdateSharepic();
+            }
+        }
+
+        private string topImagePath;
+        public string TopImagePath
+        {
+            get
+            {
+                return topImagePath;
+            }
+            set
+            {
+                topImagePath = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TopImagePath)));
+                UpdateSharepic();
+            }
+        }
+
+        private string bottomImagePath;
+        public string BottomImagePath
+        {
+            get
+            {
+                return bottomImagePath;
+            }
+            set
+            {
+                bottomImagePath = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(BottomImagePath)));
+                UpdateSharepic();
+            }
+        }
+
+        private RenderTargetBitmap sharepic;
+        public RenderTargetBitmap Sharepic
+        {
+            get
+            {
+                return sharepic;
+            }
+            set
+            {
+                sharepic = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Sharepic)));
+            }
+        }
+
+        private Dictionary<string, BackgroundSettingEnum> backgroundSettings;
+        public Dictionary<string, BackgroundSettingEnum> BackgroundSettings
+        {
+            get
+            {
+                return backgroundSettings;
+            }
+            set
+            {
+                backgroundSettings = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(BackgroundSettings)));
+            }
+        }
+
+        private KeyValuePair<string, BackgroundSettingEnum> selectedBackgroundSettings;
+        public KeyValuePair<string, BackgroundSettingEnum> SelectedBackgroundSettings
+        {
+            get
+            {
+                return selectedBackgroundSettings;
+            }
+            set
+            {
+                selectedBackgroundSettings = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedBackgroundSettings)));
+
+                UpdateConfig();
+
+                if (value.Value == BackgroundSettingEnum.TopScreenBlurred & !(TopImagePath == "" | TopImagePath == null))
+                    UpdateSharepic();
+                else if (value.Value == BackgroundSettingEnum.BottomScreenBlurred & !(BottomImagePath == "" | BottomImagePath == null))
+                    UpdateSharepic();
+                else if (value.Value == BackgroundSettingEnum.Transparent)
+                    UpdateSharepic();
+            }
+        }
+
+        private string border;
+        public string Border
+        {
+            get
+            {
+                return border;
+            }
+            set
+            {
+                border = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Border)));
+
+                UpdateConfig();
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            allowConfigUpdate = false;
+
+            this.DataContext = this;
+
+            BackgroundSettings = new Dictionary<string, BackgroundSettingEnum>();
+            BackgroundSettings.Add("Transparent", BackgroundSettingEnum.Transparent);
+            BackgroundSettings.Add("Top Screen (blurred)", BackgroundSettingEnum.TopScreenBlurred);
+            BackgroundSettings.Add("Bottom Screen (blurred)", BackgroundSettingEnum.BottomScreenBlurred);
+            SelectedBackgroundSettings = BackgroundSettings.ElementAt(0);
+
+            string profileDirectory = System.IO.Path.Combine(Environment.CurrentDirectory, "profiles");
+
+            if (!Directory.Exists(profileDirectory))
+                Directory.CreateDirectory(profileDirectory);
+
+            string[] xmlFiles = Directory.GetFiles(profileDirectory, "*.xml");
+
+            foreach (string xmlFile in xmlFiles)
+            {
+                try
+                {
+                    Profile p = new Profile(xmlFile);
+                    Profiles.Add(p);
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.MessageBox.Show("An error has occured while loading the profile \"" + xmlFile + "\":\n\n" + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+
+            if (Profiles.Count() == 0)
+            {
+                System.Windows.MessageBox.Show("No profiles found. Please download and add profiles to LumaShare.", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+                this.Close();
+            }
+
+            LoadConfig();
+        }
+
+        private void UpdateSharepic()
+        {
+            if (SelectedProfile != null)
+            {
+                try
+                {
+                    Sharepic = LumaSharepicRenderer.RenderSharepic(topImagePath, bottomImagePath, selectedProfile, SelectedBackgroundSettings.Value,  Convert.ToInt32(Border));
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.MessageBox.Show("An error has occured while generating the sharepic:\n\n" + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void btnPathSelectTop_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Top Screen Screenshots|*_top.bmp|All supported image files|*.png;*.bmp;*.jpg";
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TopImagePath = openFileDialog.FileName;
+
+                CheckForSecondImageFile(openFileDialog.FileName);
+            }
+        }
+
+        private void btnPathSelectBottom_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Bottom Screen Screenshots|*_bot.bmp|All supported image files|*.png;*.bmp;*.jpg";
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                BottomImagePath = openFileDialog.FileName;
+
+                CheckForSecondImageFile(openFileDialog.FileName);
+            }
+        }
+
+        private void CheckForSecondImageFile(string fileName)
+        {
+            if (fileName.Contains("_bot") & (TopImagePath == null || TopImagePath == ""))
+            {
+                string tempTopImage = fileName.Replace("_bot", "_top");
+
+                if (File.Exists(tempTopImage))
+                    TopImagePath = tempTopImage;
+            }
+            else if (fileName.Contains("_top") & (BottomImagePath == null || BottomImagePath == ""))
+            {
+                string tempBottomImage = fileName.Replace("_top", "_bot");
+
+                if (File.Exists(tempBottomImage))
+                    BottomImagePath = tempBottomImage;
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            TopImagePath = "";
+            BottomImagePath = "";
+        }
+
+        private static readonly Regex _regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
+        private static bool IsTextAllowed(string text)
+        {
+            return !_regex.IsMatch(text);
+        }
+
+        private void tbxBorder_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        private void tbxBorder_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(String)))
+            {
+                String text = (String)e.DataObject.GetData(typeof(String));
+                if (!IsTextAllowed(text))
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+
+        private void btnRefreshBorder_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateSharepic();
+        }
+
+        private void btnSavePicture_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "PNG file|*.png";
+            sfd.DefaultExt = "png";
+
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                BitmapFrame frame = BitmapFrame.Create(Sharepic);
+                PngBitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(frame);
+
+                using (var stream = File.Create(sfd.FileName))
+                {
+                    encoder.Save(stream);
+                }
+            }
+        }
+
+        private void LoadConfig()
+        {
+            if (File.Exists(configFileName))
+            {
+                string[] config = File.ReadAllLines(configFileName);
+
+                SelectedProfile = Profiles.Find(x => x.ProfileName == config[0].Split("=")[1]);
+                SelectedBackgroundSettings = BackgroundSettings.SingleOrDefault(x => x.Value.ToString() == config[1].Split("=")[1]);
+                Border = config[2].Split("=")[1];
+
+                allowConfigUpdate = true;
+            }
+            else
+            {
+                SelectedProfile = Profiles.ElementAt(0);
+                SelectedBackgroundSettings = BackgroundSettings.ElementAt(0);
+                Border = "0";
+
+                allowConfigUpdate = true;
+                UpdateConfig();
+            }
+        }
+
+        private void UpdateConfig()
+        {
+            if (allowConfigUpdate)
+            {
+                string[] config = new string[] { "SelectedProfile=" + SelectedProfile.ProfileName, "SelectedBackgroundSettings=" + SelectedBackgroundSettings.Value.ToString(), "Border=" + Border };
+
+                File.WriteAllLines(configFileName, config);
+            }
+        }
+    }
+}
